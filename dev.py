15b16f48a8fae43ff00ca311dde6fc5059150ddc
ChicknTurtle <chickenturtle321@gmail.com>
2024-11-27 08:16:57 -0800

    Fix error spam 2

--- dev.py
+++ dev.py
@@ -18,8 +18,8 @@ async def handle_dev(bot:discord.Bot, msg:discord.Message):
     
     args = msg.content.split(' ')
     # split second arg by newline
-    parts = args[1].split('\n', 1)
     try:
+        parts = args[1].split('\n', 1)
         args[1] = parts[0]
         if len(parts) > 1:
             args.insert(2, parts[1])
