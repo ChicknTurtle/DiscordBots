0fcac73f81eb8e5920b80d8225a37caa49cc4a78
ChicknTurtle <chickenturtle321@gmail.com>
2024-12-01 12:29:52 -0800

    Update extensionloader.py

--- extensionloader.py
+++ extensionloader.py
@@ -4,14 +4,21 @@ import discord
 
 from utils import *
 
+config = config()
+
 Log = Log()
 
-def loadext(bot:discord.Bot, files:list):
-    for file in files:
-        try:
-            bot.load_extension(f"ext.{file}")
-        except Exception as e:
-            Log.error(f"Failed to load extension: 'ext.{file}' for {bot.name}")
-            Log.error(traceback.format_exc())
-
-    Log.log(f"Loaded extensions for {bot.name}.")
+def load_exts(bot:discord.Bot):
+    ext_key = f"ext_{bot.name.lower()}"
+    exts:list = config.get(ext_key)
+    exts.append(bot.name.lower())
+    if exts:
+        for ext in exts:
+            try:
+                bot.load_extension(f"ext.{ext}")
+            except Exception as e:
+                Log.error(f"Failed to load extension: 'ext.{ext}' for {bot.name}")
+                Log.error(traceback.format_exc())
+        Log.log(f"Loaded extensions for {bot.name}.")
+    else:
+        Log.warn(f"No extensions found for bot '{bot.name}'")
