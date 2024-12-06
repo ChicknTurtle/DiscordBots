3f3c6e10b9b17aaa1b6e10ed2e4ba6144cb3694b
ChicknTurtle <chickenturtle321@gmail.com>
2024-12-06 09:41:23 -0800

    /ai image improvements
    10s cooldown, send instead of edit if original message was deleted
    
    and bot feedback doesn't mention

--- ext/tuttlebot/ai.py
+++ ext/tuttlebot/ai.py
@@ -1,8 +1,10 @@
 
+import time
 import datetime
 import aiohttp
 import io
 import discord
+from discord.ext import commands
 
 from utils import *
 
@@ -21,21 +23,26 @@ def setup(bot):
                 return await response.read()
 
     # image
-    @bot_group.command(name="image",description="Generate an image using ai",)
-    async def ai_image_command(ctx:discord.ApplicationContext, prompt:str):
+    @bot_group.command(name="image",description="Generate an image using ai")
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def ai_image_command(ctx:discord.ApplicationContext, prompt=discord.Option(str, max_length=1000, description="Image prompt")):
         original = await ctx.respond(f"**Generating Image...**\nPrompt: `{prompt}`")
-
         start_time = datetime.now() 
         image_bytes = await query({"inputs": prompt})
         end_time = datetime.now()
         total_time = end_time - start_time
-
         image_io = io.BytesIO(image_bytes)
-
         try:
-            await original.edit_original_response(content=f"{prompt}\n**Generated in {format_time(total_time)}**", file=discord.File(fp=image_io, filename="generated_image.png"))
-        except Exception as e:
-            await original.edit_original_response(content=f"**Generating Image...**\nThere was an error! The API may have broken.")
-            raise(e)
-    # add help description
+            await original.edit_original_response(content=f"{prompt}\n**Generated in {format_time(total_time)}**", file=discord.File(fp=image_io, filename="generated_image.png"), allowed_mentions=discord.AllowedMentions.none())
+        except discord.NotFound:
+            image_io.seek(0)
+            await ctx.send(f"-# {ctx.author.mention} used /ai image\n{prompt}\n**Generated in {format_time(total_time)}**", file=discord.File(fp=image_io, filename="generated_image.png"), allowed_mentions=discord.AllowedMentions.none())
     ai_image_command.helpdesc="[model used](<https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0>)"
+    # image error
+    @ai_image_command.error
+    async def ai_image_command_error(ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            cooldowntime = math.ceil(time.time()+error.retry_after)
+            await ctx.respond(f"You're on cooldown! Try again <t:{cooldowntime}:R>.", ephemeral=True)
+        else:
+            raise error
